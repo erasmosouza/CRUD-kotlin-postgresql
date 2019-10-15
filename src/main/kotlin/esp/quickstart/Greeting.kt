2b5db1da2b846b9c226e8a7287b9453867097654
erasmo.pinheiro <erasmosouza@gmail.com>
2019-10-15 11:41:58 -0300

    Correções e criação de um basico Health Check

--- src/main/kotlin/esp/quickstart/Greeting.kt
+++
@@ -1,5 +0,0 @@
-package esp.quickstart
-
-data class Greeting(val id: Long, val content: String)
-{
-}
---
+++ src/main/kotlin/esp/quickstart/Health.kt
@@ -0,0 +1,5 @@
+package esp.quickstart
+
+data class Health(val id: Long, val content: String)
+{
+}
